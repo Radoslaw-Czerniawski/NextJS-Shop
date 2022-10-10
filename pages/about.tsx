f3397cd6a579311795569f0a2e69a761181b1954
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-10-10 20:07:04 +0200

    add tailwindcss, change app name, add about page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface AboutProps {}
+
+const AboutPage = (props: AboutProps) => {
+    return (
+        <h1 className="text-3xl font-bold underline text-red-400">
+            About Page
+        </h1>
+    );
+};
+
+export default AboutPage;
