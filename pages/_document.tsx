a27a21903d800fda364d1a5003de260b2c9bdce8
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-10-10 20:51:18 +0200

    add some experimental styling and basic html structure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,8 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html>
-            <Head lang="pl" />
-            <body>
+            <Head lang='pl' />
+            <body className='bg-gray-300 antialiased'>
                 <Main />
                 <NextScript />
             </body>
--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,7 @@ interface AboutProps {}
 
 const AboutPage = (props: AboutProps) => {
     return (
-        <h1 className="text-3xl font-bold underline text-red-400">
+        <h1 className='text-3xl font-bold text-center border-4 text-red-400'>
             About Page
         </h1>
     );
