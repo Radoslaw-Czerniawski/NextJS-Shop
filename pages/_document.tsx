0ddd530ddaa53870b2430a7c6eaa8f24aac908ca
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-10-10 18:13:31 +0200

    add document component to set lang to polish

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+        <Html>
+            <Head lang="pl" />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
