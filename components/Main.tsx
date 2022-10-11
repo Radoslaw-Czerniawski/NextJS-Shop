8a35fba4df65b394df813d3a0ff4461206ce34b0
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-10-11 11:18:41 +0200

    remove Main component and extract its content to index.tsx

--- components/Main.tsx
+++
@@ -1,7 +0,0 @@
-export const Main = () => {
-    return (
-        <main className='flex justify-center items-center flex-grow max-w-md'>
-            <section className='text-4xl p-5'>Main Section</section>
-        </main>
-    );
-};
