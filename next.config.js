9cf25684d90b93b1521f5041c213c180c086e137
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-10-11 11:25:02 +0200

    export text to variable, correct Image component props and config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     reactStrictMode: false,
     swcMinify: true,
     trailingSlash: true,
+    images: {
+        domains: ['picsum.photos'],
+    },
 };
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,23 @@
 import Image from 'next/image';
 
+const DATA = {
+    description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quiesse dolorum iste veritatis porro ratione aliquid vel mollitia voluptatum deleniti laborum obcaecati numquam sit, culpa ipsam officia saepe quis.',
+};
+
 const Home = () => {
     return (
         <main className='flex flex-col gap-10 flex-grow max-w-2xl mx-auto p-6 sm:grid sm:grid-cols-2 sm:gap-6'>
             <div className='w-full h-auto justify-center'>
                 <Image
+                    width={536}
+                    height={354}
                     className='w-full'
                     src='https://picsum.photos/id/1060/536/354'
                     alt='random photo'
                 />
             </div>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non qui
-                esse dolorum iste veritatis porro ratione aliquid vel mollitia
-                voluptatum deleniti laborum obcaecati numquam sit, culpa ipsam
-                officia saepe quis.
-            </p>
+            <p>{DATA.description}</p>
         </main>
     );
 };
