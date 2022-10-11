c8cf511f7a125add788c19d994173c59f56528cf
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-10-11 17:37:45 +0200

    add procuts page, remove uniqid

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,16 @@ import { useRouter } from 'next/router';
 
 const paths = [
     {
-        key: uniqid(),
         text: 'Main',
         path: '/',
     },
     {
-        key: uniqid(),
         text: 'About',
         path: '/about',
+    },
+    {
+        text: 'Products',
+        path: '/products',
     },
 ];
 
@@ -21,8 +23,8 @@ export const Header = () => {
     return (
         <header className='flex bg-gray-500 justify-center w-full'>
             <nav className='flex text-4xl gap-4'>
-                {paths.map(({ key, text, path }) => (
-                    <Link key={key} href={path}>
+                {paths.map(({ text, path }) => (
+                    <Link key={text} href={path}>
                         <a
                             className={
                                 router.pathname === path ? 'font-bold' : ''
