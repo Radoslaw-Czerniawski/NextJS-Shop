b1ab7b681f349c4e08fd4d9618ab85c287ecdea9
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-10-13 21:19:53 +0200

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,24 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html>
-            <Head lang='pl' />
+            <Head lang='pl'>
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                    }}
+                />
+            </Head>
             <body className='antialiased'>
                 <Main />
                 <NextScript />
