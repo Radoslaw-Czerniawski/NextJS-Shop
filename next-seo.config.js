1f93ccfc1a50f33c089415a10984be4b5215aab7
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-10-24 20:14:11 +0200

    install next-seo and next-mdx-remote, add default seo head tags

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+const title = 'Mój Sklep Next Shop';
+const description =
+    'Aplikacja Testowa powstała w celu zrozumienia działania Next.js';
+
+const nextSeoConfig = {
+    title,
+    description,
+    openGraph: {
+        title,
+        description,
+        site_name: 'Next Shop',
+    },
+};
+
+export default nextSeoConfig;
